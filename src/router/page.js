8eb3f398668b73b237ef90c26fd1e471f54c1dc0
Viktoriya <step.vikulya@mail.ru>
2023-03-06 22:08:39 +0700

    Рефакторинг роутера

---
+++ src/router/page.js
@@ -0,0 +1,68 @@
+import Header from "../../src/components/header/header";
+import Banner from "../../src/components/banner/banner";
+import Blog from "../../src/components/blog/blog";
+import About from "../../src/components/about/about";
+import ArticlePage from "../../src/components/article/article";
+import PageError from "../../src/components/404/404";
+
+
+class Pages {
+  #header = new Header("header", "header container");
+  #banner = new Banner("section", "banner container");
+  #bannerMove = new Banner("section", "banner banner--move container");
+  #blog = new Blog("section", "blog container");
+  #about = new About("section", "about container");
+  #article = new ArticlePage("section", "article container");
+  #error = new PageError("section", "error container");
+
+  constructor() {
+    this.container = document.createElement("div");
+    this.container.id = "app";
+  }
+
+  async home() {
+    this.container.append(
+      await this.#header.render(),
+      await this.#banner.render(),
+      await this.#blog.render(3),
+      await this.#bannerMove.render(true)
+    );
+  }
+  
+  async blog() {
+    this.container.append(
+      await this.#header.render(),
+      await this.#banner.render(),
+      await this.#blog.render()
+    );
+  }
+
+  async about() {
+    this.container.append(
+      await this.#header.render(),
+      await this.#about.render()
+    );
+  }
+
+  async article() {
+    this.container.append(
+      await this.#header.render(),
+      await this.#article.render()
+    );
+  }
+
+  async error() {
+    this.container.append(
+      await this.#header.render(),
+      await this.#error.render()
+    );
+  }
+
+  async render() {
+    document.body.append(this.container);
+    document.getElementById("spinner").style.display = "none";
+  }
+}
+
+
+export default Pages;
--- src/router/router.js
+++ src/router/router.js
@@ -1,110 +1,34 @@
-import generatePage from "../templates/generatePage";
-import Header from "../../src/components/header/header";
-import Banner from "../../src/components/banner/banner";
-import Blog from "../../src/components/blog/blog";
-import About from "../../src/components/about/about";
-import ArticlePage from "../../src/components/article/article";
-import PageError from "../../src/components/404/404";
 import Navigo from "navigo";
+import Pages from "./page";
 
-const header = new Header("header", "header container");
-const banner = new Banner("section", "banner container");
-const bannerMove = new Banner("section", "banner banner--move container");
-const blog = new Blog("section", "blog container");
-const about = new About("section", "about container");
-const article = new ArticlePage("section", "article container");
-const error = new PageError("section", "error container");
 const router = new Navigo("/");
-
-const appendPage = (page) => {
-  document.getElementById("spinner").style.display = "none";
-  document.body.append(page);
-  burger()
-};
-
-const pages = {
-  homePage: async () => {
-    return new generatePage(
-      await header.render(),
-      await banner.render(),
-      await blog.render(3),
-      await bannerMove.render(true)
-    );
-  },
-  blogPage: async () => {
-    return new generatePage(
-      await header.render(),
-      await banner.render(),
-      await blog.render()
-    );
-  },
-  aboutPage: async () => {
-    return new generatePage(
-        await header.render(), 
-        await about.render()
-    );
-  },
-  articlePage: async () => {
-    return new generatePage(
-        await header.render(), 
-        await article.render()
-    );
-  },
-  errorPage: async () => {
-    return new generatePage(
-        await header.render(), 
-        await error.render()
-    );
-  },
-};
+const page = new Pages();
 
 router
   .on({
     "/": async () => {
-      const page = await pages.homePage();
-      appendPage(page.render());
-    },
-
-    "/about": async () => {
-      const page = await pages.aboutPage();
-      appendPage(page.render());
+      await page.home();
+      await page.render();
     },
 
     "/blog": async () => {
-      const page = await pages.blogPage();
-      appendPage(page.render());
+      await page.blog();
+      await page.render();
+    },
+
+    "/about": async () => {
+      await page.about();
+      await page.render();
     },
 
     "/blog/article/:id": async () => {
-      const page = await pages.articlePage();
-      appendPage(page.render());
+      await page.article();
+      await page.render();
     },
   })
-  .notFound( async() => {
-    const page = await pages.errorPage();
-    appendPage(page.render());
+  .notFound( async () => {
+    await page.error();
+    await page.render();
   })
   .resolve();
-
-
-
-  function burger() {
-    const navigationBlock = document.getElementById("nav");
-    const burger = document.getElementById("burger");
-  
-    burger.addEventListener("click", (e) => {
-      // e.preventDefault();
-      navigationBlock.classList.toggle("open");
-    });
-  
-    document.addEventListener("click", (e) => {
-      if (
-        navigationBlock.classList.contains("open") &&
-        e.target != navigationBlock
-      ) {
-        console.log("ok");
-        // navigationBlock.classList.remove("open");
-      }
-    });
-  }
   
