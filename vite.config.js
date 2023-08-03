4e0ab1b5dd8a4044c7f08f624cc47abdcdb7a3c5
Viktoriya <step.vikulya@mail.ru>
2023-08-03 16:53:04 +0700

    Редактирование стилей

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ export default defineConfig({
   base: "./",
   server: {
     port: 5000,
-  }
+  },
+  css: {
+  preprocessorOptions: {
+    scss: {
+      additionalData: `@import "/src/styles/_const.scss", "/src/styles/_mixins.scss";`,
+    },
+  },
+},
 });
